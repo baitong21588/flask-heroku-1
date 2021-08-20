03dbbb153e6701105796afd8bac2c01234c7f68d
baitong21588 <87843312+baitong21588@users.noreply.github.com>
2021-08-20 09:30:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_api():
 
 @app.route('/name')
 def name():
-        return "<font color=red>สุภณิดา หุตะวัฒนะ</font> <br> เลขที่ 21 ชั้น ม.4/10"
+        return "<font color=pink>สุภณิดา หุตะวัฒนะ</font> <br> เลขที่ 21 ชั้น ม.4/10"
 
 if __name__ == "__main__":
     app.run(debug=False)
